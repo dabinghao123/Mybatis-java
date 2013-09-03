17065980b27199f3049037a3bdb0979b7826fd8d
huangbing <huangbing@handsem.com>
2013-09-03 16:23:55 +0800

    post is commit

---
+++ src/com/test/demo/Post.java
@@ -0,0 +1,30 @@
+public  Post{
+
+public  Post(){
+
+
+}
+private  String title;
+private  String  content;
+private   Date  date;
+
+
+private  void setTitle(String title){
+	this.title=title;
+}
+private  String getTitle(){
+
+  return title;
+
+}
+
+private  void setContent(String content){
+	this.content=content;
+}
+private  String getContent(){
+
+  return content;
+
+}
+
+}
