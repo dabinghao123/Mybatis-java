6fd7607befb497bd04cb91f9ad3d2ad1d6334ade
huangbing <408181273@qq.com>
2013-09-03 16:02:22 +0800

    haha

---
+++ src/com/test/demo/Demo.java
@@ -0,0 +1,30 @@
+package com.test.demo;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String resource = "mybatis-config.xml";
+		InputStream inputStream = null;
+		try {
+			inputStream = Resources.getResourceAsStream(resource);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+             SqlSession openSession = sqlSessionFactory.openSession(); 
+               System.out.println(openSession);
+	}
+
+}
